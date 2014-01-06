11c81543ea5acacc2ec7dfc80c2fde309ad1d27c
Sandeep <sandeepbeit@gmail.com>
2014-01-05 21:01:57 -0500

    price info

--- app/controllers/bitcoin_prices_controller.rb
+++ app/controllers/bitcoin_prices_controller.rb
@@ -14,7 +14,8 @@ class BitcoinPricesController < ApplicationController
 
     coinbase = Coinbase::Client.new("5d6e5a7891b129eea78841b5995be2c762883b14709b2cc2575f79f767e8ce24")
 
-    @coinbase = coinbase.buy_price(1)
+    @coinbase_buy = coinbase.buy_price(1).format
+    @coinbase_sell = coinbase.sell_price(1).format
 
   end
 
